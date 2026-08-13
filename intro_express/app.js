650a43103081a4ff58c79f9f222846390b3fb89f
jhonalexbeltran2008 <jhonalexbeltran2008@gmail.com>
2026-08-13 17:12:51 -0500

    enpoint post creado

--- intro_express/app.js
+++ intro_express/app.js
@@ -1,26 +1,55 @@
-import 'dotenv/config';
-import express from 'express';
-const app = express();
-const port = process.env.PORT || 3000;
-app.get("/", (_, res) => {
-    res.send("Hola , estamos aprendiendo express con la ficha 3407184");
+const express = require('express');  
+const app = express();  
+const port = 3000; 
+
+app.use(express.json());
+
+const listaAprendices = [
+    { nombre: "Oscar Andrés", edad: 19, correo: "oscar@example.com", imgPerfil: "https://x.com/La_Tmb_/status/1625196711143477256" },
+    { nombre: "Juan David", edad: 20, correo: "juan@example.com", imgPerfil: "https://www.amazon.com.mx/Quinten-Massys-duquesa-laminado-pulgadas/dp/B0DQYK4WZ1" },
+    { nombre: "Camilo", edad: 21, correo: "camilo@example.com", imgPerfil: "https://unsplash.com/es/s/fotos/gente-fea" }
+];
+
+app.get("/", function(req, res) {
+    res.send("Hola, estamos aprendiendo express con la ficha 3407184"); 
+});  
+
+// Agrega esta ruta para responder a GET /api/aprendices
+app.get("/api/aprendices", function(req, res) {
+    res.json(listaAprendices);
 });
 
-app.get("/json", (_, res) => {
-    res.json({
-        datos_personales: {
-            nombre: "Esteban",
-            apellido: "Reyes",
-            telefonos: ["3006706756"],
-        },
-        datos_programa: {
-            nombre: "ADSO",
-            tipo_programa: "Tecnologo",
-            ficha: "3407184",
-        },
-    });
+app.get("/api/aprendices/:nombre", function(req, res) {
+    const nombreRecibido = req.params.nombre.toLowerCase();
+
+    const datosAprendiz = listaAprendices.filter(aprendiz => 
+        aprendiz.nombre.toLowerCase().includes(nombreRecibido)
+    );
+    if (datosAprendiz.length > 0) {
+        res.json({ busqueda: req.params.nombre, resultados: datosAprendiz });
+    } else {
+        res.status(404).json({ error: "Aprendiz no encontrado" });
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+app.post("/api/aprendices", function(req, res) {
+    const { nombre, edad, correo, imgPerfil } = req.body;
+
+    // Validar nombre: mínimo 3 letras
+    if (!nombre || nombre.length < 3) {
+        return res.status(400).json({ error: "El nombre debe tener mínimo 3 letras" });
+    }
+
+    // Validar correo electrónico: debe contener @ y .
+    if (!correo || !correo.includes("@") || !correo.includes(".")) {
+        return res.status(400).json({ error: "El correo electrónico no es válido" });
+    }
+
+    const nuevoAprendiz = { nombre, edad, correo, imgPerfil };
+    listaAprendices.push(nuevoAprendiz);
+    res.status(201).json({ mensaje: "Aprendiz creado con éxito", Datos: nuevoAprendiz });
+});
+
+app.listen(port, function() { 
+    console.log(`SERVIDOR: http://localhost:${port}`); 
 });
